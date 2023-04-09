bdd4c48ce2786bb5ae1277ad68ba928d9f00ef77
Aman Shukla <aman54108s@gmail.com>
2023-04-09 09:48:15 +0530

    Create areaofrectangle.java

---
+++ areaofrectangle.java
@@ -0,0 +1,10 @@
+public class Main
+{
+  public static void main(String[] args) {
+    int l=6, b=5;
+    System.out.println("The length is : "+ l);
+    System.out.println("The breadth is : "+ b);
+    int area=l*b;
+    System.out.println("The area is : "+area);
+  }
+}
