1b3340b17db27d0f33380531966a1bb8382a8647
Aman Shukla <aman54108s@gmail.com>
2023-04-09 09:49:19 +0530

    Create numberscube.java

---
+++ numberscube.java
@@ -0,0 +1,9 @@
+public class Main
+{
+  public static void main(String[] args) {
+    int x = 2;
+    int cube=x*x*x;
+    System.out.println("The side is : "+x);
+    System.out.println("The cube is : "+cube);
+  }
+}
